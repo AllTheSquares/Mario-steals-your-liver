0c7340f7ba46cd05f9c6f875d6e062ad2e3f39fe
AllTheSquares <74791172+AllTheSquares@users.noreply.github.com>
2022-08-02 16:35:11 -0400

    Contents and scripts of the program
    These files are all required for the program to function.

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # Mario steals your liver
- Silly program that makes the joke video into a real thing. 
+Silly program that makes the joke video into a real thing. 
+
+This repository contains the contents and the scripts required for this program to work. You can build this program on your own time, or you can probably check branches.
+
+Be warned: Mario truly means it when he says he will get your liver. By all means be sure to save everything important before executing this program (either built or unbuilt), as your computer WILL SHUT DOWN once all is done. LAUNCH AT YOUR OWN RISK!
+
+This program was built on Windows for Windows in mind.
+
+Features: 
+ > Initial Tkinter popup that looks like a Windows error as seen in the commonly-shared image on the internet.
+ > You cannot close out of the program without using Task Manager. 
+ > The cancel button doesn't work. You have no other choice.
+ > Clicking the OK button will start a random timer. It will take up to a minute for the main payload to execute.
+ > Your computer will shut down at the conclusion of the payload. 
---
+++ marioLiverAnim.py
@@ -0,0 +1,84 @@
+# Written by AllTheSquares, program based off of this video: https://www.youtube.com/watch?v=QjWzjw6m20U
+
+# Imports
+from tkinter import *
+from PIL import Image, ImageSequence, ImageTk
+import os # for shutdown command
+from pygame import mixer # audio system
+
+# Functions
+def standing_Mario(): # Swaps initial image of Mario kicking a hole through your screen to him standing asking for your liver
+    global marioStand
+
+    # Sets up the standing pose for Mario to be displayed after the kick is removed
+    marioStand = PhotoImage(file = 'Contents\marioStand.png') # Pulls image from contents folder
+    marioStand = marioStand.zoom(2,2) # Enlargens image
+
+    canvas.delete(Mario) # Deletes original instance of Mario (kicking pose)
+    canvas.create_image(canvasWidth/2,canvasHeight/2, image = marioStand, anchor=CENTER) # creates new canvas image at the same position of the original
+    canvas.after(3500, hand_zoom)
+
+def hand_zoom():
+    global handFrame, handCanvas
+
+    handCanvas = Canvas(handFrame, width = canvasWidth, height = canvasHeight)
+    handCanvas.pack()
+
+    img = Image.open("Contents\hand.gif") 
+    hand = Label(handFrame) # Creates a label that will refresh per frame of the GIF.
+    handCanvas.create_window(canvasWidth/3, canvasHeight/3, window = hand) # Places the GIF right near Mario's head as per the original meme video. 
+    
+    for img in ImageSequence.Iterator(img): # This for loop refreshes the gif for each frame (50)
+        img = ImageTk.PhotoImage(img)
+        hand.config(image = img)
+        hand.update()
+    
+    # Functions to do after the zoom sequence ends: computer shutdown, audio stop
+    handCanvas.after(0, shutdown_Computer)
+    handCanvas.after(0,mixer.music.stop)
+
+def shutdown_Computer():
+    root.destroy()
+    os.system("shutdown /s /t 1")
+
+def start_anim(): # called by the original script (liver.py) after a random amount of time
+    global root, canvas, hole, canvasWidth, canvasHeight, Mario, handFrame
+
+    # Pygame Audio (plays the audio from the original video)
+    mixer.init() 
+    mixer.music.load("Contents\MarioLiver.wav")
+    mixer.music.play(-1)
+
+    # Sets up primary window of the "animation". This window will contain the hole in your screen that Mario creates after kicking through it.
+    root = Tk()
+    root.attributes('-transparentcolor','#f0f0f0')
+    root.attributes('-fullscreen', True)
+    root.eval('tk::PlaceWindow . center')
+    root.geometry("1000x1000")
+    root.overrideredirect(1)
+
+    # This creates a toplevel window of the original Tkinter GUI instance used for the hole and Mario. This is for the gif of the hand zooming into your screen.
+    handFrame = Toplevel(root)
+    handFrame.geometry("1000x1000")
+    handFrame.attributes('-transparentcolor','#f0f0f0')
+    handFrame.attributes('-fullscreen', True)
+    handFrame.overrideredirect(1)
+
+    # Specifies height and width of Canvas
+    canvasWidth = 1000
+    canvasHeight = 1000
+
+    # Original source images found on the internet (made transparent with remove.bg)
+    hole = PhotoImage(file = "Contents\hole.png")
+    marioKick = PhotoImage(file = "Contents\marioKick.png")
+
+    # Adds the hole image AND Mario himself
+    canvas = Canvas(root, width = canvasWidth, height = canvasHeight)
+    canvas.create_image(canvasWidth/2,canvasHeight/2, image=hole, anchor=CENTER)
+    Mario = canvas.create_image(canvasWidth/2,canvasHeight/2, image=marioKick, anchor=CENTER)
+    canvas.pack(anchor="center")
+
+    # Execute next function/next part of the "animation"
+    canvas.after(1000, standing_Mario)
+
+    root.mainloop()
